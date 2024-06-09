84498cb96bdd455d36968f7da72a53b75f605888
Andy Baranov <andrey@hey.com>
2024-06-09 12:53:55 +0200

    disabley key and salt

--- config/initializers/imgproxy.rb
+++ config/initializers/imgproxy.rb
@@ -3,8 +3,8 @@ if Rails.env == "production"
     # Full URL to where your imgproxy lives.
     config.endpoint = "https://imgproxy.progapanda.org"
     # Hex-encoded signature key and salt
-    config.key = ENV["IMGPROXY_KEY"]
-    config.salt = ENV["IMGPROXY_SALT"]
+    # config.key = ENV["IMGPROXY_KEY"]
+    # config.salt = ENV["IMGPROXY_SALT"]
   end
 else
   Imgproxy.configure do |config|
